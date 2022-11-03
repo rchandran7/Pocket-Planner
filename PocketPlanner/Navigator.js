bb851d27893cbc54b32bfe9e04fb8d8c00680d0d
Bilal Nasir <bnasi004@ucr.edu>
2022-11-03 15:48:27 -0700

    tidying up navigation units

---
+++ PocketPlanner/Navigator.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+const Tab = createBottomTabNavigator();
+
+function ScreenNavigator() {
+  return (
+    <NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen}/>
+            <Tab.Screen name="Settings" component={SettingsScreen}/>
+        </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default ScreenNavigator;
