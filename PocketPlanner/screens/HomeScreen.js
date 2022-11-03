e6a5cad85a2412be0c8b0f41c6a59e8a5b4f6d74
Bilal Nasir <bnasi004@ucr.edu>
2022-11-03 16:18:46 -0700

    start of the event view on home screen

--- PocketPlanner/screens/HomeScreen.js
+++ PocketPlanner/screens/HomeScreen.js
@@ -1,10 +1,22 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-export default function HomeScreen({ navigation }) {
+export default function HomeScreen() {
     return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home</Text>
+    <View style = {styles.container}>
+
+        <Text>Todays Events</Text> 
+
     </View>
     );
   }
+
+
+  const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+  })
