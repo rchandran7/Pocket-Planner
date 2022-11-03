1be52e6f0121323c230a064598cb402596c2dafc
unknown <jonclarke94@gmail.com>
2022-11-03 06:58:37 -0700

    added custom inputs
    Username and password

--- PocketPlanner/src/Screens/SignInScreen.js
+++ PocketPlanner/src/Screens/SignInScreen.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-cont SignInScreen = () => {
+const SignInScreen = () => {
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const {height} = useWindowDimensions();
 return (
-<View>
-<Text> Sign In Screen</Text>
-</View>
-)
-}
+    <View style = {styles.root}>
+        
+        <CustomInput 
+        placeholder = "Username" 
+        value = {username} 
+        setValue = {setUsername}
+        />
+        <CustomInput 
+        placeholder = "Password" 
+        value = {password} 
+        setValue = {setPassword} 
+        secureTextEntry ={true}
+        />
+    </View>
+    );
+};
 
+const styles = Style.Sheet.create({
+    root: {
+        alignItems:'center',
+        padding: 20,
+    },
+})
 export default SignInScreen
