497726deda9b7a67016412584b022299b8849b31
unknown <jonclarke94@gmail.com>
2022-11-27 14:32:38 -0800

    asdfawerqwer

--- PocketPlanner/src/Screens/SignUpScreen/SignUpScreen.js
+++ PocketPlanner/src/Screens/SignUpScreen/SignUpScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import CustomInput from '../../Component/CustomInput/CustomInput';
 import CustomButton from '../../Component/CustomButton/CustomButton';
+import SocialSignInButtons from '../../Component/SocialSignInButtons/SocialSignInButtons';
 import {useState} from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import SocialSignInButtons from '../../Component/SocialSignInButtons/SocialSignInButtons';
+
 
 const SignUpScreen = () => {
     const [username,setUsername] = useState('');
@@ -11,68 +12,63 @@ const SignUpScreen = () => {
     const [password,setPassword] = useState('');
     const [passwordRepeat, setPasswordRepeat] = useState('');
 
-const onRegisterPressed = () => {
+    const onRegisterPressed = () => {
     console.warn("Register Pressed");
-}
+    };
 
-const onTermsOfUsePressed = () => {
+    const onTermsOfUsePressed = () => {
     console.warn("Terms Of Use Pressed");
-}
+    };
 
-const onPrivacyPressed = () => {
+    const onPrivacyPressed = () => {
     console.warn("Privacy Policy Pressed");
-}
+    };
 
-return (
-    <ScrollView showsVerticalScrollIndicator = {false}>
-        <View style = {styles.root}>
-          <Text style = {style.title}> Create An Account </Text>
+    return (
+        <ScrollView showsVerticalScrollIndicator = {false}>
+            <View style = {styles.root}>
+                <Text style = {styles.title}> Create An Account </Text>
 
-        <CustomInput 
-        placeholder = "Username" 
-        value = {username} 
-        setValue = {setUsername}
-        />
-        <CustomInput 
-        placeholder = "Email" 
-        value = {email} 
-        setValue = {setEmail}
-        />
+                <CustomInput 
+                    placeholder = "Username" 
+                    value = {username} 
+                    setValue = {setUsername}
+                />
+                <CustomInput 
+                    placeholder = "Email" 
+                    value = {email} 
+                    setValue = {setEmail}
+                />
 
-        <CustomInput 
-        placeholder = "Password" 
-        value = {password} 
-        setValue = {setPassword} 
-        secureTextEntry = {true}
-        />
-        <CustomInput 
-        placeholder = "Repeat Password" 
-        value = {passwordRepeat} 
-        setValue = {setPasswordRepeat} 
-        secureTextEntry = {true}
-        />
+                <CustomInput 
+                    placeholder = "Password" 
+                    value = {password} 
+                    setValue = {setPassword} 
+                    secureTextEntry = {true}
+                />
+                <CustomInput 
+                    placeholder = "Repeat Password" 
+                    value = {passwordRepeat} 
+                    setValue = {setPasswordRepeat} 
+                    secureTextEntry = {true}
+                />
 
-        <CustomButton 
-            text = "Register" 
-            onPress ={onRegisterPressed}
-        />
+                <CustomButton 
+                    text = "Register" 
+                    onPress ={onRegisterPressed}
+                />
 
-        <Text style = {styles.text}>
-            By Registering, you confirm that you accept our{' '}
-            <Text styles = {styles.link} onPress = {onTermsOfUsePressed}>Terms of Use</Text>
-             and {' '}
-            <Text styles = {styles.link} onPress = {onPrivacyPressed}>Privacy Policy</Text>
-        </Text>
+                <Text style = {styles.text}>
+                    By Registering, you confirm that you accept our{' '}
+                    <Text styles = {styles.link} onPress = {onTermsOfUsePressed}>Terms of Use</Text>
+                    and {' '}
+                    <Text styles = {styles.link} onPress = {onPrivacyPressed}>Privacy Policy</Text>
+                </Text>
 
-        <SocialSignInButtons/>
+                <SocialSignInButtons/>
 
-        <CustomButton 
-            text = "Don't have an account? Create One" 
-            onPress ={onSignUpPressed}
-            type = "TERTIARY"
-        />
-    </View>
-    </ScrollView>
+            </View>
+        </ScrollView>
     );
 };
 
@@ -94,4 +90,5 @@ const styles = StyleSheet.create({
         margin: 10,
     }
 });
+
 export default SignUpScreen
