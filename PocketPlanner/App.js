e30bfa7e701ea1171656de8e17a508502d3d36a0
Bilal Nasir <95551739+noshotbigjuicersbilal151@users.noreply.github.com>
2023-02-02 14:07:36 -0800

    sign up config

--- PocketPlanner/App.js
+++ PocketPlanner/App.js
@@ -1,13 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet, Text, Image, View } from 'react-native';
-import SignInScreen from './src/Screens/SignInScreen';
-import SignUpScreen from './src/Screens/SignUpScreen';
-import { NavigationContainer } from '@react-navigation/native';
-
-import ScreenNavigator from './src/Screens/Navigator'
 import DefaultStack from './src/Routes/defaultStack'
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import {withAuthenticator} from 'aws-amplify-react-native';
 import config from './src/aws-exports';
 
