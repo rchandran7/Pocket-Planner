e93b46353e978bcd82b06be444c063a70cbe192d
Bilal Nasir <95551739+noshotbigjuicersbilal151@users.noreply.github.com>
2023-02-02 13:12:10 -0800

    fixed sign up config

--- PocketPlanner/App.js
+++ PocketPlanner/App.js
@@ -15,9 +15,43 @@ import config from './src/aws-exports';
 Amplify.configure(config);
 function App() {
   return (
-    // <DefaultStack />
     <DefaultStack />
   );
 };
 
-export default withAuthenticator(App); 
+const signUpConfig = {
+  header: "My Customized Sign Up",
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: "Name",
+      key: "name",
+      required: true,
+      displayOrder: 1,
+      type: "string",
+    },
+    {
+      label: "Email",
+      key: "email",
+      required: true,
+      displayOrder: 2,
+      type: "string",
+    },
+    {
+      label: "Username",
+      key: "preferred_username",
+      required: true,
+      displayOrder: 3,
+      type: "string",
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 4,
+      type: "password",
+    },
+  ],
+};
+
+export default withAuthenticator(App, {signUpConfig}); 
