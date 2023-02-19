5722f2379ac30118b91684b3ce098c090954713a
Bilal <bnasi004@ucr.edu>
2023-02-19 15:56:26 -0800

    TRYING MY BEST

--- PocketPlanner/App.js
+++ PocketPlanner/App.js
@@ -1,12 +1,14 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
 import DefaultStack from './src/Routes/defaultStack'
-import { Amplify, Auth } from 'aws-amplify';
+import { Amplify, Auth} from 'aws-amplify';
 import {withAuthenticator} from 'aws-amplify-react-native';
 import config from './src/aws-exports';
 
 
 Amplify.configure(config);
+
 function App() {
+  
   return (
     <DefaultStack />
   );
