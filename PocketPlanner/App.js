f88b1cbb23c810e068b8da2db37626ea670994df
Bilal Nasir <95551739+noshotbigjuicersbilal151@users.noreply.github.com>
2023-02-21 15:17:03 -0800

    config + user linking

--- PocketPlanner/App.js
+++ PocketPlanner/App.js
@@ -1,14 +1,47 @@
 import React, {useEffect} from 'react';
 import DefaultStack from './src/Routes/defaultStack'
-import { Amplify, Auth} from 'aws-amplify';
+import { Amplify, Auth, API, graphqlOperation} from 'aws-amplify';
 import {withAuthenticator} from 'aws-amplify-react-native';
 import config from './src/aws-exports';
+import {getUser} from './src/graphql/queries';
+import {createUser} from './src/graphql/mutations';
 
 
 Amplify.configure(config);
 
 function App() {
+  useEffect(() => {
+    const syncUser = async () => {
+      // get Auth user
+      const authUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      // query the database using Auth user id (sub)
+      const userData = await API.graphql(
+        graphqlOperation(getUser, { id: authUser.attributes.sub })
+      );
+
+      if (userData.data.getUser) {
+        console.log("User already exists in DB");
+        return;
+      }
+      // if there is no users in db, create one
+      const newUser = {
+        id: authUser.attributes.sub,
+        name: authUser.attributes.name,
+        bio: "Hey, I am using PocketPlanner",
+      };
+
+      await API.graphql(
+        graphqlOperation(createUser, { input: newUser })
+      );
+    };
+
+    syncUser();
+  }, []);
   
+
   return (
     <DefaultStack />
   );
