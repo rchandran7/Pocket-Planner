a121c4ac624d4aa8a0cd25550de5c1b1b33dcc87
rchandran7 <rchan121@ucr.edu>
2023-02-23 15:20:49 -0800

    Added nested modals

--- PocketPlanner/src/Component/ScheduleList/AddInput.js
+++ PocketPlanner/src/Component/ScheduleList/AddInput.js
@@ -6,10 +6,23 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
+import { useCallback } from 'react/cjs/react.production.min';
 
 export default function AddInput({submitHandler}) {
-  const navigation = useNavigation();
   const [modalVisible, setModalVisible] = useState(false);
+  const [classModalVisible, setClassModalVisible] = useState(false);
+  const [addClassModalVisible, setAddClassModalVisible] = useState(false);
+  const [assignmentModalVisible, setAssignmentModalVisible] = useState(false);
+  const [meetingModalVisible, setMeetingModalVisible] = useState(false);
+  // const onOpenClassModal = useCallback(() => {
+  //   setModalVisible(false);
+  //   setTimeout(
+  //     () => {
+  //       setClassModalVisible(true);
+  //     },
+  //     100,
+  //   );
+  // }, []);
   return (
     <View style={styles.centeredView}>
       <Modal
@@ -21,19 +34,75 @@ export default function AddInput({submitHandler}) {
           setModalVisible(!modalVisible);
         }}>
         <View style = {styles.modalView}>
-          <ClassButton onPress={()=>setModalVisible(!modalVisible)}>
+          <ClassButton onPress={()=> setClassModalVisible(true)}>
             <TextClass>Create Class</TextClass>
           </ClassButton>
-          <AddClassButton onPress={()=>setModalVisible(!modalVisible)}>
+          <AddClassButton onPress={()=>setAddClassModalVisible(true)}>
             <TextAddClass>Add Class</TextAddClass>
           </AddClassButton>
-          <AssignmentButton onPress={()=>setModalVisible(!modalVisible)}>
+          <AssignmentButton onPress={()=>setAssignmentModalVisible(true)}>
             <TextAssignment>Add Assignment</TextAssignment>
           </AssignmentButton>
-          <MeetingButton onPress={()=>setModalVisible(!modalVisible)}>
+          <MeetingButton onPress={()=>setMeetingModalVisible(true)}>
             <TextMeeting>Add Meeting</TextMeeting>
           </MeetingButton>
         </View>
+        <Modal
+          animationType="fade"
+          transparent = {true}
+          visible = {classModalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setClassModalVisible(!classModalVisible);
+          }}>
+          <View style = {styles.classModalView}>
+            <CloseModalsButton onPress={()=>{setModalVisible(!modalVisible); setClassModalVisible(!classModalVisible)}}>
+              <TextClassModal>Temp: Create class modal here</TextClassModal>
+            </CloseModalsButton>
+          </View>
+        </Modal>
+        <Modal
+          animationType="fade"
+          transparent = {true}
+          visible = {addClassModalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setAddClassModalVisible(!addClassModalVisible);
+          }}>
+          <View style = {styles.addClassModalView}>
+            <CloseModalsButton onPress={()=>{setModalVisible(!modalVisible); setAddClassModalVisible(!addClassModalVisible)}}>
+              <TextClassModal>Temp: Add class modal Here</TextClassModal>
+            </CloseModalsButton>
+          </View>
+        </Modal>
+        <Modal
+          animationType="fade"
+          transparent = {true}
+          visible = {assignmentModalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setAssigmentModalVisible(!assignmentModalVisible);
+          }}>
+          <View style = {styles.assignmentModalView}>
+            <CloseModalsButton onPress={()=>{setModalVisible(!modalVisible); setAssignmentModalVisible(!assignmentModalVisible)}}>
+              <TextClassModal>Temp: Add assignment modal here</TextClassModal>
+            </CloseModalsButton>
+          </View>
+        </Modal>
+        <Modal
+          animationType="fade"
+          transparent = {true}
+          visible = {meetingModalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setAssigmentModalVisible(!meetingModalVisible);
+          }}>
+          <View style = {styles.meetingModalView}>
+            <CloseModalsButton onPress={()=>{setModalVisible(!modalVisible); setMeetingModalVisible(!meetingModalVisible)}}>
+              <TextClassModal>Temp: Add meeting modal here</TextClassModal>
+            </CloseModalsButton>
+          </View>
+        </Modal>
       </Modal>
       <PlusButton onPress={()=>setModalVisible(true)}>
         <ButtonContainer>
@@ -42,7 +111,7 @@ export default function AddInput({submitHandler}) {
       </PlusButton>
     </View>
   );
-}
+};
 
 
 const styles = StyleSheet.create({
@@ -51,6 +120,70 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   modalView: {
+    marginTop: 110,
+    margin: 55,
+    backgroundColor: '#B0C4DE',
+    borderRadius: 20,
+    padding: 40,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  classModalView: {
+    marginTop: 110,
+    margin: 55,
+    backgroundColor: '#B0C4DE',
+    borderRadius: 20,
+    padding: 40,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  addClassModalView: {
+    marginTop: 110,
+    margin: 55,
+    backgroundColor: '#B0C4DE',
+    borderRadius: 20,
+    padding: 40,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  assignmentModalView: {
+    marginTop: 110,
+    margin: 55,
+    backgroundColor: '#B0C4DE',
+    borderRadius: 20,
+    padding: 40,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  meetingModalView: {
     marginTop: 110,
     margin: 55,
     backgroundColor: '#B0C4DE',
@@ -115,4 +248,14 @@ const TextMeeting = styled.Text`
   font-size: 25px;
   padding-top: 50px;
   color: #FF4500;
+`;
+const TextClassModal = styled.Text`
+  text-align: center;
+  font-size: 25px;
+  color: #FF4500;
+`;
+
+const  CloseModalsButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
 `;
